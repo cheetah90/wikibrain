d6f8493b17c90d3e09a4e93f7d264eecce90906b
tobyli <li@toby.li>
2015-04-07 20:06:04 -0500

    modified get local page

--- wikibrain-cookbook/src/main/java/org/wikibrain/cookbook/core/GetLocalPage.java
+++ wikibrain-cookbook/src/main/java/org/wikibrain/cookbook/core/GetLocalPage.java
@@ -1,5 +1,6 @@
 package org.wikibrain.cookbook.core;
 
+import au.com.bytecode.opencsv.CSVReader;
 import au.com.bytecode.opencsv.CSVWriter;
 import com.vividsolutions.jts.geom.Geometry;
 import org.wikibrain.conf.Configurator;
@@ -13,8 +14,10 @@ import org.wikibrain.core.model.LocalPage;
 import org.wikibrain.core.model.NameSpace;
 import org.wikibrain.spatial.dao.SpatialDataDao;
 
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -31,7 +34,8 @@ public class GetLocalPage {
         UniversalPageDao upDao = conf.get(UniversalPageDao.class);
         Map<Integer, Geometry> allGeomMap = sdDao.getAllGeometriesInLayer("wikidata");
         Language lang = Language.EN;
-        CSVWriter writer = new CSVWriter(new FileWriter("talk_page_id.csv"), ',');
+        CSVReader reader = new CSVReader(new FileReader("talk_page_id.csv"), ',');
+        CSVWriter writer = new CSVWriter(new FileWriter("talk_page_id_2.csv"), ',');
         String row[] = new String[5];
         row[0] = "LOCAL_ID";
         row[1] = "TITLE";
@@ -41,22 +45,36 @@ public class GetLocalPage {
         writer.writeNext(row);
         writer.flush();
 
+
         int i = 0;
         int errorCount = 0;
-        for(Map.Entry<Integer, Geometry> entry : allGeomMap.entrySet()){
+        List<String[]> readRows = reader.readAll();
+        for(String[] readRow : readRows){
             if((i++) % 100 == 0)
-                System.out.println("DONE " + i + " OUT OF " + allGeomMap.entrySet().size() + " error " + errorCount);
+                System.out.println("DONE " + i + " OUT OF " + readRows.size() + " error " + errorCount);
             try{
-                Integer univId = entry.getKey();
-                Integer localId = upDao.getLocalId(lang, univId);
-                String title = lpDao_sql.getById(lang, localId).getTitle().getCanonicalTitle();
-                String talkTitle = "Talk:" + title;
-                Integer talkId = lpDao_live.getIdByTitle(talkTitle, lang, NameSpace.TALK);
-                row[0] = localId.toString();
-                row[1] = title;
+                if(!readRow[2].contentEquals("-1"))
+                    continue;
+                String talkTitle = readRow[3];
+                int failCount = 0;
+                boolean failed = true;
+                Integer talkId = -1;
+                while(failCount++ < 5){
+                    talkId = lpDao_live.getIdByTitle(talkTitle, lang, NameSpace.TALK);
+                    if(talkId != -1){
+                        failed = false;
+                        break;
+                    }
+                }
+                if(failed){
+                    System.out.println("Failed to get talk page for  " + readRow[1]);
+                    throw  new Exception();
+                }
+                row[0] = readRow[0];
+                row[1] = readRow[1];
                 row[2] = talkId.toString();
                 row[3] = talkTitle;
-                row[4] = entry.getValue().toString();
+                row[4] = readRow[4];
                 writer.writeNext(row);
                 writer.flush();
             }
@@ -65,6 +83,8 @@ public class GetLocalPage {
                 continue;
             }
         }
+
+
         writer.flush();
         writer.close();
 
