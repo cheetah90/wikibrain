d9b20436b665894d1d80f425f5992e91ec186bac
tobyli <li@toby.li>
2015-04-17 15:55:14 -0500

    autocomplete fix

--- atlasify/src/main/java/org/wikibrain/atlasify/AtlasifyResource.java
+++ atlasify/src/main/java/org/wikibrain/atlasify/AtlasifyResource.java
@@ -781,7 +781,7 @@ public class AtlasifyResource {
             autocompleteCache.put(query.getKeyword(), autocompleteMap);
         }
 
-        System.out.println("Get Auto Complete Result" + new JSONObject(new autoCompeleteResponse(autocompleteMap, query.getChecksum())).toString());
+        System.out.println("Get Auto Complete Result" + new JSONObject(new autoCompeleteResponse(autocompleteMap, query.getChecksum()), new String[] { "resultList", "autoCompleteChecksum" }).toString());
         return Response.ok(new JSONObject(new autoCompeleteResponse(autocompleteMap, query.getChecksum())).toString()).build();
     }
     public String getExplanation(String keyword, String feature) throws Exception{
