f3152a365af68607d56d300605ede6f90e87cfca
tobyli <li@toby.li>
2015-05-14 00:04:12 -0500

    change entropy

--- wikibrain-cookbook/src/main/java/org/wikibrain/cookbook/core/WikiEntropyCalculator.java
+++ wikibrain-cookbook/src/main/java/org/wikibrain/cookbook/core/WikiEntropyCalculator.java
@@ -56,80 +56,101 @@ public class WikiEntropyCalculator {
         scDao = c.get(SpatialContainmentDao.class);
         llDao = c.get(LocalLinkDao.class);
 
-        Language lang = Language.EN;
-        CSVWriter writer = new CSVWriter(new FileWriter(lang.getLangCode() + "_countyEntropy.csv"), ',');
-        String[] row = new String[4];
-        row[0] = "Universal ID";
-        row[1] = "Name";
-        row[2] = "Entropy";
-        row[3] = "County Article Count";
-        writer.writeNext(row);
-        writer.flush();
-        CSVReader reader = new CSVReader(new FileReader("gadm_matched.csv"), ',');
-        List<String[]> gadmList = reader.readAll();
-        for(String[] gadmItem : gadmList){
-            if(Integer.parseInt(gadmItem[2]) < 2)
-                GADM01Concepts.add(Integer.parseInt(gadmItem[0]));
-        }
-        Map<Integer, Geometry> countyMap = sdDao.getAllGeometriesInLayer("counties");
-        int countyCounter = 0;
-        for(Map.Entry<Integer, Geometry> countyEntry : countyMap.entrySet()){
-            System.out.println("Done with " + countyCounter++ + " counties out of " + countyMap.size());
 
-            Set<String> layerSet = new HashSet<String>();
-            layerSet.add("wikidata");
-            List<LocalLink> allCountyLinks = new ArrayList<LocalLink>();
-            TIntSet resultSet = scDao.getContainedItemIds(countyEntry.getValue(), "earth", layerSet, SpatialContainmentDao.ContainmentOperationType.CONTAINMENT);
-            TIntIterator resultIterator = resultSet.iterator();
-            Double infoEntropy = 0.0;
-            int countyArticleCounter = 0;
-            while (resultIterator.hasNext()){
-                Integer univId = resultIterator.next();
-                countyArticleCounter ++;
-                if(GADM01Concepts.contains(univId))
-                    continue;
-                Integer localId = upDao.getLocalId(lang, univId);
-                if(localId == -1)
-                    //no corresponding local page in the given language
-                    continue;
-                Iterable<LocalLink> localLinks = llDao.getLinks(lang, localId, true);
-                Iterator<LocalLink> localLinkIterator = localLinks.iterator();
-                while (localLinkIterator.hasNext()){
-                    allCountyLinks.add(localLinkIterator.next());
-                }
-            }
-            //calculate entropy
-            List<LocalLink> sampleList = getRandomSubList(allCountyLinks, allCountyLinks.size() > 1000 ? 1000 : allCountyLinks.size());
-            Map<Integer, Integer> linkDestinationCountMap = new HashMap<Integer, Integer>();
-            for(LocalLink l : sampleList){
-                if(linkDestinationCountMap.containsKey(l.getDestId())){
-                    linkDestinationCountMap.put(l.getDestId(), linkDestinationCountMap.get(l.getDestId()) + 1);
-                }
-                else{
-                    linkDestinationCountMap.put(l.getDestId(), 1);
-                }
-            }
-            for(Map.Entry<Integer, Integer> linkDestinationCountEntry : linkDestinationCountMap.entrySet()){
-                Double selfInformation = -(Math.log(linkDestinationCountEntry.getValue().doubleValue() / sampleList.size()) / Math.log(2));
-                infoEntropy += (selfInformation * (linkDestinationCountEntry.getValue().doubleValue() / sampleList.size()));
-            }
-            row[0] = countyEntry.getKey().toString();
-            row[1] = "N/A";
-            try {
-                row[1] = upDao.getById(countyEntry.getKey()).getBestEnglishTitle(lpDao, true).getCanonicalTitle();
-            }
-            catch (Exception e){
-                System.out.println("error in processing " + countyEntry.getKey());
+        Set<Language> langs = new HashSet<Language>();
+        langs.add(Language.EN);
+        langs.add(Language.ZH);
+        langs.add(Language.DE);
+        langs.add(Language.EN);
+        langs.add(Language.ES);
+        langs.add(Language.JA);
+        langs.add(Language.KO);
+        langs.add(Language.NO);
+        langs.add(Language.PL);
+        langs.add(Language.RU);
+        langs.add(Language.VI);
 
-            }
-            row[2] = infoEntropy.toString();
-            row[3] = String.valueOf(countyArticleCounter);
+        for(Language lang : langs){
+            CSVWriter writer = new CSVWriter(new FileWriter(lang.getLangCode() + "_countyEntropy.csv"), ',');
+            String[] row = new String[7];
+            row[0] = "Universal ID";
+            row[1] = "Name";
+            row[2] = "Entropy";
+            row[3] = "County Article Count";
+            row[4] = "Total Outlinks Count";
+            row[5] = "Sample Unique Outlink Count";
+            row[6] = "Sample Size";
             writer.writeNext(row);
             writer.flush();
+            CSVReader reader = new CSVReader(new FileReader("gadm_matched.csv"), ',');
+            List<String[]> gadmList = reader.readAll();
+            for(String[] gadmItem : gadmList){
+                if(Integer.parseInt(gadmItem[2]) < 2)
+                    GADM01Concepts.add(Integer.parseInt(gadmItem[0]));
+            }
+            Map<Integer, Geometry> countyMap = sdDao.getAllGeometriesInLayer("counties");
+            int countyCounter = 0;
+            for(Map.Entry<Integer, Geometry> countyEntry : countyMap.entrySet()){
+                System.out.println("Done with " + countyCounter++ + " counties out of " + countyMap.size());
+
+                Set<String> layerSet = new HashSet<String>();
+                layerSet.add("wikidata");
+                List<LocalLink> allCountyLinks = new ArrayList<LocalLink>();
+                TIntSet resultSet = scDao.getContainedItemIds(countyEntry.getValue(), "earth", layerSet, SpatialContainmentDao.ContainmentOperationType.CONTAINMENT);
+                TIntIterator resultIterator = resultSet.iterator();
+                Double infoEntropy = 0.0;
+                int countyArticleCounter = 0;
+                while (resultIterator.hasNext()){
+                    Integer univId = resultIterator.next();
+                    countyArticleCounter ++;
+                    if(GADM01Concepts.contains(univId))
+                        continue;
+                    Integer localId = upDao.getLocalId(lang, univId);
+                    if(localId == -1)
+                        //no corresponding local page in the given language
+                        continue;
+                    Iterable<LocalLink> localLinks = llDao.getLinks(lang, localId, true);
+                    Iterator<LocalLink> localLinkIterator = localLinks.iterator();
+                    while (localLinkIterator.hasNext()){
+                        allCountyLinks.add(localLinkIterator.next());
+                    }
+                }
+                //calculate entropy
+                List<LocalLink> sampleList = getRandomSubList(allCountyLinks, allCountyLinks.size() > 1000 ? 1000 : allCountyLinks.size());
+                Map<Integer, Integer> linkDestinationCountMap = new HashMap<Integer, Integer>();
+                for(LocalLink l : sampleList){
+                    if(linkDestinationCountMap.containsKey(l.getDestId())){
+                        linkDestinationCountMap.put(l.getDestId(), linkDestinationCountMap.get(l.getDestId()) + 1);
+                    }
+                    else{
+                        linkDestinationCountMap.put(l.getDestId(), 1);
+                    }
+                }
+                for(Map.Entry<Integer, Integer> linkDestinationCountEntry : linkDestinationCountMap.entrySet()){
+                    Double selfInformation = -(Math.log(linkDestinationCountEntry.getValue().doubleValue() / sampleList.size()) / Math.log(2));
+                    infoEntropy += (selfInformation * (linkDestinationCountEntry.getValue().doubleValue() / sampleList.size()));
+                }
+                row[0] = countyEntry.getKey().toString();
+                row[1] = "N/A";
+                try {
+                    row[1] = upDao.getById(countyEntry.getKey()).getBestEnglishTitle(lpDao, true).getCanonicalTitle();
+                }
+                catch (Exception e){
+                    System.out.println("error in processing " + countyEntry.getKey());
+
+                }
+                row[2] = infoEntropy.toString();
+                row[3] = String.valueOf(countyArticleCounter);
+                row[4] = String.valueOf(allCountyLinks.size());
+                row[5] = String.valueOf(linkDestinationCountMap.size());
+                row[6] = String.valueOf(sampleList.size());
+                writer.writeNext(row);
+                writer.flush();
 
 
+            }
+            writer.close();
         }
-        writer.close();
 
     }
 }
