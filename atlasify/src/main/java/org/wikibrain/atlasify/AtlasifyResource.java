ecd4c6b357c09123a71825e50ae5dea4531efa19
tobyli <li@toby.li>
2015-06-17 11:51:34 -0500

    switch bing key

--- atlasify/src/main/java/org/wikibrain/atlasify/AtlasifyResource.java
+++ atlasify/src/main/java/org/wikibrain/atlasify/AtlasifyResource.java
@@ -1070,7 +1070,7 @@ public class AtlasifyResource {
             } */
 
             /* Bing */
-            String bingAccountKey = "qe8udhnGIOmt7tBb6uLXEBfdfykn7OnMssQW1HXlYKo";
+            String bingAccountKey = "gpLKu1VxcX0yo0Vbkyr6BZHwFiJjZR2Y6t7qiMsnD0s=";
             byte[] bingAccountKeyBytes = Base64.encodeBase64((bingAccountKey + ":" + bingAccountKey).getBytes());
             String bingAccountKeyEncoded = new String(bingAccountKeyBytes);
 
