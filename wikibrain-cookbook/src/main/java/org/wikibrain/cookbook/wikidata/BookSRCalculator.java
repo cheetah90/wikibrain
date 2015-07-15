d40c552f76c60c4a5c99c8b434471ae7add3ad57
tobyli <li@toby.li>
2015-07-15 13:49:15 -0500

    disable sr

--- wikibrain-cookbook/src/main/java/org/wikibrain/cookbook/wikidata/BookSRCalculator.java
+++ wikibrain-cookbook/src/main/java/org/wikibrain/cookbook/wikidata/BookSRCalculator.java
@@ -7,6 +7,7 @@ import org.wikibrain.core.cmd.EnvBuilder;
 import org.wikibrain.core.dao.DaoException;
 import org.wikibrain.core.dao.LocalPageDao;
 import org.wikibrain.core.dao.UniversalPageDao;
+import org.wikibrain.core.lang.Language;
 import org.wikibrain.sr.SRMetric;
 import org.wikibrain.wikidata.WikidataDao;
 import org.wikibrain.wikidata.WikidataValue;
@@ -19,21 +20,23 @@ import java.util.Set;
  * Created by toby on 7/15/15.
  */
 public class BookSRCalculator {
-
-    public static void main(String args[]) throws FileNotFoundException, IOException, ConfigurationException, DaoException {
-        Env env = new EnvBuilder().build();
+    private static SRMetric sr;
+    private static UniversalPageDao upDao;
+    private static LocalPageDao lpDao;
+    public BookSRCalculator(Env env, Language lang) throws ConfigurationException{
         Configurator conf = env.getConfigurator();
-        UniversalPageDao upDao = conf.get(UniversalPageDao.class);
-        LocalPageDao lpDao = conf.get(LocalPageDao.class);
-        //SRMetric sr = conf.get(SRMetric.class, "ensemble","language", "en");
-        WikidataDao wdDao = conf.get(WikidataDao.class);
-        Set<Integer> conceptSet = wdDao.conceptsWithValue("instance of", WikidataValue.forItem(5));
-        int count = 0;
-        for(Integer id : conceptSet){
-            System.out.println(id);
-            if(count ++ > 50)
-                break;
-        }
+        sr = conf.get(SRMetric.class, "ensemble", "language", lang.getLangCode());
+        upDao =  conf.get(UniversalPageDao.class);
+        lpDao = conf.get(LocalPageDao.class);
+    }
+    public double getSR(Integer universalId1, Integer universalId2, Language lang) throws DaoException{
+        return sr.similarity(lpDao.getById(lang, upDao.getLocalId(lang, universalId1)).getTitle().getCanonicalTitle(), lpDao.getById(lang, upDao.getLocalId(lang, universalId2)).getTitle().getCanonicalTitle(), false).getScore();
+    }
 
+    public static void main(String args[]) throws ConfigurationException, DaoException {
+        Language lang = Language.EN;
+        Env env = EnvBuilder.envFromArgs(args);
+        BookSRCalculator calculator = new BookSRCalculator(env, lang);
+        calculator.getSR(89, 503, lang);
     }
 }
